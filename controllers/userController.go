bdd5a5ad941cf2e7e6745a64f239af18d98df576
pi-rate14 <apoorva.srivastava2019@vitstudent.ac.in>
2022-02-11 03:31:58 +0530

    userType helpers

--- controllers/userController.go
+++ controllers/userController.go
@@ -1,5 +1,18 @@
 package controllers
 
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/go-playground/validator/v10"
+	"github.com/pi-rate14/golang-jwt-project/database"
+	helper "github.com/pi-rate14/golang-jwt-project/helpers"
+	"go.mongodb.org/mongo-driver/mongo"
+)
+
+var userCollection *mongo.Collection = database.OpenCollection(database.Client, "user")
+var validate = validator.New()
+
 func HashPassword() 
 
 func VerifyPassword()
@@ -10,5 +23,14 @@ func Login()
 
 func GetUser()
 
-func GetUsers()
+func GetUsers() gin.HandlerFunc{
+	return func(c *gin.Context){
+		userId := c.Param("user_id")
+		err := helper.MatchUserTypeToUid(c, userId)
+		if err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
+	}
+}
 
